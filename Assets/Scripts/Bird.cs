ad1aa9666a8ae0fa5582a33d0c607db2131f8cb2
valdeci <valdecidanilo@live.com>
2022-05-09 10:11:01 -0300

    Up

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [Range(0f, 500f)]public float velocity;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public float time;
+    public float distance;
+    public Text txtDistance;
+    void Update()
+    {
+        distance += 0.1f * Time.deltaTime;
+        txtDistance.text = distance.ToString("0.00");
+    }
+}
