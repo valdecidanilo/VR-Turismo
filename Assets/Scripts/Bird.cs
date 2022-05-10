1d1c2c4866fcdc855ca3fdf3c151424399a06878
valdeci <valdecidanilo@live.com>
2022-05-10 09:18:13 -0300

    update

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [Range(0f, 500f)]public float velocity;
-    void Update()
-    {
+    Rigidbody rb;
+	float dirX;
+	float moveSpeed = 20f;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update () {
+		dirX = Input.acceleration.x * moveSpeed;
         transform.Translate(Vector3.forward * velocity * Time.deltaTime);
-    }
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -7.5f, 7.5f), transform.position.y, transform.position.z);
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector3 (dirX, 0f, velocity * Time.deltaTime);
+	}
 }
