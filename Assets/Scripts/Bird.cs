5118f2efe8f385c04d496f5221691155edab5c5d
valdeci <valdecidanilo@live.com>
2022-05-17 07:45:23 -0300

    new Assets

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [Range(0f, 500f)]public float velocity;
+	public Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
     Rigidbody rb;
 	float dirX;
 	float moveSpeed = 20f;
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		//GyroManager.Instance.EnabledGyro();
 	}
 	void Update () {
-		dirX = Input.acceleration.x * moveSpeed;
-        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
-		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -7.5f, 7.5f), transform.position.y, transform.position.z);
+		//dirX = Input.acceleration.x * moveSpeed;
+        //transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+		//transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -50f, 50f), transform.position.y, transform.position.z);
+		//transform.localRotation = GyroManager.Instance.GetGyroRotation() * baseRotation;
 	}
 
 	void FixedUpdate()
 	{
-		rb.velocity = new Vector3 (dirX, 0f, velocity * Time.deltaTime);
+		rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y, velocity * Time.deltaTime);
 	}
 }
