b8688a637babba26bc4a10dae3d9e8bc75544f38
valdeci <valdecidanilo@live.com>
2022-05-18 09:35:28 -0300

    Added Fixed move

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -15,13 +15,18 @@ public class Bird : MonoBehaviour
 	}
 	void Update () {
 		//dirX = Input.acceleration.x * moveSpeed;
-        //transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
 		//transform.position = new Vector3 (Mathf.Clamp (transform.position.x, -50f, 50f), transform.position.y, transform.position.z);
 		//transform.localRotation = GyroManager.Instance.GetGyroRotation() * baseRotation;
+	}
+	private void OnTriggerEnter(Collider other) {
+		if(other.tag == "Point"){
+			Destroy(other.gameObject);
+		}
 	}
 
 	void FixedUpdate()
 	{
-		rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y, velocity * Time.deltaTime);
+		//rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y, velocity * Time.deltaTime);
 	}
 }
