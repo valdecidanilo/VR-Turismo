edf2dfebb05aa28c92ba9795ed29edd145e947c3
valdeci <valdecidanilo@live.com>
2022-05-26 22:06:41 -0300

    Squashed commit of the following:
    commit cec4261a0080f7ab72775a68e53fcceed7c43ed3
    Author: valdeci <valdecidanilo@live.com>
    Date:   Thu May 26 22:02:18 2022 -0300
    
        Squashed commit of the following:
    
        commit 9d45819c934528e5d655d579f6e0595d78b7a5cc
        Author: Igor <moonjkji@gmail.com>
        Date:   Mon May 23 00:21:52 2022 -0300
    
            Merge_23-05
    
            Adicionadas modelos das vegetações compartilhando textura e Modelo da Casa daVinicola e FabricaVinicola
    
        commit 312f7369e1a7467b3bb653dff227204f744b1370
        Author: Igor <moonjkji@gmail.com>
        Date:   Sun May 22 00:53:31 2022 -0300
    
            Merge_22-05
    
            Adicionados modelos das arvores e plantas com texturas juntas, queijos e barco final.
    
        commit 7c5f7fb0fb8e9b1236d47a00732313baee0b3aff
        Author: Igor <moonjkji@gmail.com>
        Date:   Thu May 19 22:42:24 2022 -0300
    
            Merge_19-05
    
            Adicionei algumas coisas nada que faça alguma diferença kkkk.
    
    commit ebd76850598bbb2a9ed81e7a0956f4f9821fe4cd
    Author: valdeci <valdecidanilo@live.com>
    Date:   Thu May 26 14:00:21 2022 -0300
    
        fixeds loops blocks
    
    commit 90c10f15b6c836750d3fccbfb704d29c7ef23cb5
    Author: valdeci <valdecidanilo@live.com>
    Date:   Thu May 26 13:47:14 2022 -0300
    
        fixed blocks added
    
    commit 91a4d4ded44832d19acb872f8f9d5259c8aed94f
    Author: valdeci <valdecidanilo@live.com>
    Date:   Mon May 23 08:37:31 2022 -0300
    
        Scene Update
    
    commit 3b4dfb4b0d4bca4520829c1d6a1b4cec19ac0c8d
    Author: valdeci <valdecidanilo@live.com>
    Date:   Sun May 22 16:14:19 2022 -0300
    
        Update game Logic
    
    commit e6c3146a8470e80bedc096cffd54e7f18f817e47
    Author: valdeci <valdecidanilo@live.com>
    Date:   Sat May 21 21:41:13 2022 -0300
    
        fixed animation
    
    commit ae1ebea4b8a3b3a76163e378726e8d86807fdd25
    Author: valdeci <valdecidanilo@live.com>
    Date:   Sat May 21 18:18:54 2022 -0300
    
        fixed map test
    
    commit 54f52763db6b6f6abb0a3ea3aa8ff7d1727836d4
    Author: valdeci <valdecidanilo@live.com>
    Date:   Thu May 19 17:28:57 2022 -0300
    
        fixed move
    
    commit b8688a637babba26bc4a10dae3d9e8bc75544f38
    Author: valdeci <valdecidanilo@live.com>
    Date:   Wed May 18 09:35:28 2022 -0300
    
        Added Fixed move
    
    commit 5118f2efe8f385c04d496f5221691155edab5c5d
    Author: valdeci <valdecidanilo@live.com>
    Date:   Tue May 17 07:45:23 2022 -0300
    
        new Assets
    
    commit 1d1c2c4866fcdc855ca3fdf3c151424399a06878
    Author: valdeci <valdecidanilo@live.com>
    Date:   Tue May 10 09:18:13 2022 -0300
    
        update

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,12 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Bird : MonoBehaviour
 {
     [Range(0f, 500f)]public float velocity;
-    void Update()
+    Rigidbody rb;
+	private bool gyroActive, defense;
+	public int maxWidth;
+	public float timerDefense;
+	private Gyroscope gyro;
+	public GameManager gameManager;
+	public GameObject ptcDefense;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		EnabledGyro();
+	}
+	public void EnabledGyro()
     {
-        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        if(gyroActive){ return; }
+        if(SystemInfo.supportsGyroscope){
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            gyroActive = gyro.enabled;
+        }
+        
     }
+	void Update () {
+		Move();
+		if(defense){
+			if(timerDefense > 0){
+				timerDefense -= 1f * Time.deltaTime;
+			}else{
+				defense = false;
+				timerDefense = 0;
+				ptcDefense.SetActive(false);
+			}
+		}
+	}
+	void Move(){
+		transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+		if(gyroActive){
+			gameManager.txtDebug.text = "Rotation: " + gyro.attitude + "\n" + "Rotation Rate: " + gyro.rotationRate + "\n" + "Aceleration: " + gyro.userAcceleration;
+            GetComponentInChildren<Animator>().SetFloat("SpeedX", -gyro.rotationRate.z / 15f);
+            transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x + (-gyro.rotationRate.z / 15f),-maxWidth, maxWidth), transform.localPosition.y, transform.localPosition.z);
+        }else{
+            var vel = Input.GetAxis("Horizontal") / 15f;
+            transform.GetComponentInChildren<Animator>().SetFloat("SpeedX", vel);
+            transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x + (vel),-maxWidth, maxWidth), transform.localPosition.y, transform.localPosition.z);
+        }
+	}
+	private void OnTriggerEnter(Collider other) {
+		if(other.tag == "Point"){
+			TypePowerUp currentType = other.GetComponent<PowerUp>().type;
+			if(currentType == TypePowerUp.Speed){
+				velocity += 0.1f;
+			}else if(currentType == TypePowerUp.Defense){
+				defense = true;
+				timerDefense = 8f;
+				ptcDefense.SetActive(true);
+			}
+			Destroy(other.gameObject);
+		}
+		if(other.tag == "Block" && !defense){
+			SceneManager.LoadScene("Minigame0");
+		}
+		if(other.tag == "Create"){
+			gameManager.CreateBlock(other.transform.parent.gameObject);
+			//GameManager.createBlock?.Invoke(other.gameObject);
+		}
+	}
 }
