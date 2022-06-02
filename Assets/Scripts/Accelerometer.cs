6531ce412cc50f719e6e7a20e2fd211ad2281fa7
valdeci <valdecidanilo@live.com>
2022-06-02 08:42:30 -0300

    Squashed commit of the following:
    commit b6161d04c119d75a5d052c5e7087a7c357fa26fe
    Author: valdeci <valdecidanilo@live.com>
    Date:   Tue May 17 12:38:46 2022 -0300
    
        remove scene minigame2
    
    commit 5f68d833c539a18dc77f041bfb34f8750d4a2888
    Author: valdeci <valdecidanilo@live.com>
    Date:   Tue May 17 09:26:23 2022 -0300
    
        Added Skybox and Mecanics
    
    commit 5118f2efe8f385c04d496f5221691155edab5c5d
    Author: valdeci <valdecidanilo@live.com>
    Date:   Tue May 17 07:45:23 2022 -0300
    
        new Assets
    
    commit 1d1c2c4866fcdc855ca3fdf3c151424399a06878
    Author: valdeci <valdecidanilo@live.com>
    Date:   Tue May 10 09:18:13 2022 -0300
    
        update

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if(isFlat){
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.red);
+        rb.AddForce(tilt);
+    }
+}
