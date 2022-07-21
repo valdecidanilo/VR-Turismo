e2edd7aae0ad2d5fcde4d8f8b777b8ae3b2719ab
Mikael Cavalcanti <valdecidanilo@live.com>
2022-07-21 10:17:49 -0300

    Added Pedalinho MiniGame

---
+++ Assets/Scripts/Paths.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paths : MonoBehaviour
+{
+    public PathPoint[] paths;
+    void OnDrawGizmos() {
+        paths = transform.GetComponentsInChildren<PathPoint>();
+        if(paths.Length <= 0){ return; }
+        for (int i = 0; i < paths.Length; i++)
+        {
+            Gizmos.DrawWireSphere(paths[i].transform.position, 0.5f);
+            if(i != paths.Length-1){
+                Gizmos.DrawLine(paths[i].transform.position, paths[i + 1].transform.position);
+            }else{
+                Gizmos.DrawLine(paths[i].transform.position, paths[0].transform.position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pedalinho.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedalinho : MonoBehaviour
+{
+    [Range(1f, 50f)] public float velocity;
+    public Rigidbody rb;
+    public Paths path; 
+    public int currentPoint;
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move(){
+        if(Vector3.Distance(transform.position, path.paths[currentPoint].transform.position) < 1f){
+            currentPoint++;
+            Debug.Log("PROXIMO PATH");
+        }
+        Quaternion lookOnLook = Quaternion.LookRotation(path.paths[currentPoint].transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookOnLook, 0.05f);
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+    }
+}
